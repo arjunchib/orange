30e6fc750ecd7e6c1f0fd3c3327798e08c4f208c
Arjun Chib <arjunpatrickchib@gmail.com>
2024-08-17 06:19:49 +0000

    Add webhook

--- src/event_handlers.ts
+++ src/event_handlers.ts
@@ -7,8 +7,17 @@ export async function handlePushEvent(event: PushEvent) {
   if (repo === "peach") {
     await $`git pull`.cwd(`${Bun.env.HOME}/peach`);
   } else if (repo === "memebot3") {
-    await $`git pull && sudo systemctl restart memebot`.cwd(
+    await $`git pull && bun i && sudo systemctl restart memebot`.cwd(
       `${Bun.env.HOME}/memebot3`
     );
   }
+  await fetch(`${Bun.env.DISCORD_WEBHOOK!}?wait=true`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      content: `Deployed ${event.head_commit?.id} for ${repo}`,
+    }),
+  });
 }
