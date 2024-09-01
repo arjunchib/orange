440f855eef58b48e1d52b31149fc70741c6436c0
Arjun Chib <arjunpatrickchib@gmail.com>
2024-09-01 06:34:52 +0000

    Add project name to set env

--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,10 @@
 import { slashCommand, string, subcommand } from "peach";
+import { readdirSync } from "fs";
+
+const projects = readdirSync("../").filter((dir) => !dir.startsWith("."));
 
 export const setEnv = subcommand("set", "set environment variable").options([
+  string("project", "name of project").required().choices(projects),
   string("name", "name of variable").required(),
   string("value", "value to set").required(),
 ]);
